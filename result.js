2243eb2b3d70d644adea4b38f7d6d4bc9285d5ce
rq-Suziye-blip-at-321899771753 <hryks15gp@gmail.com>
2025-09-19 11:17:17 +0900

    mod:シェアボタンを押した際に添付されるリンクがリザルト画面になっていたのでトップページになるよう変更

--- result.js
+++ result.js
@@ -12,8 +12,8 @@ document.addEventListener('DOMContentLoaded', () => {
         twitterShareButton.className = 'twitter-share-button';
         twitterShareButton.href = 'https://twitter.com/share?ref_src=twsrc%5Etfw';
         twitterShareButton.setAttribute('data-show-count', 'false');
-        twitterShareButton.setAttribute('data-text', message + " #小さい秋_見つけろ。");
-        twitterShareButton.setAttribute('data-url', 'https://suziye-blip.github.io/find_your_little_autumn/');
+        twitterShareButton.setAttribute('data-text', message + " #小さい秋_見つけろ");
+        twitterShareButton.setAttribute('data-url', 'https://suziye-blip.github.io/find_your_little_autumn/index.html');
         twitterShareArea.appendChild(twitterShareButton);
 
         // Twitterウィジェットスクリプトを読み込み
